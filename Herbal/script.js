e48b19731d88cbe141ba76b3a607c6aa668ab6ca
odhiambow2354 <odhiambow2354@gmail.com>
2023-12-15 11:00:20 +0300

    Herbal E-store

---
+++ Herbal/script.js
@@ -0,0 +1,7 @@
+let navbar=document.querySelector('.navbar');
+		document.querySelector('#menu').onclick = () =>{
+			navbar.classList.toggle('active')
+		}
+		window.onscroll = () =>{
+			navbar.classList.remove('active')
+		}
